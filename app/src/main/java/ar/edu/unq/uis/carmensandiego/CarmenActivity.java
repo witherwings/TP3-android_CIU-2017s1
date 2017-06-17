f30bfcd33f54cf5ce15e22b18facace28c16a1c2
Lucas <lucas.wfh@gmail.com>
2017-06-17 15:39:12 -0300

    vista de juego principal hecha

--- app/src/main/java/ar/edu/unq/uis/carmensandiego/CarmenActivity.java
+++ app/src/main/java/ar/edu/unq/uis/carmensandiego/CarmenActivity.java
@@ -1,7 +1,9 @@
 package ar.edu.unq.uis.carmensandiego;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CarmenActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class CarmenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carmen);
+
+        setTitle("Carmen San Diego");
+    }
+
+    public void buttonResolverMisterio_onClick(View view) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
