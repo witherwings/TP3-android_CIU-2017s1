4a182622413713f7f2226c5842bcb568ff90fdd3
Lucas <lucas.wfh@gmail.com>
2017-06-21 18:29:53 -0300

    spinner para viajar en vez de botones

--- app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
+++ app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
@@ -1,5 +1,6 @@
 package ar.edu.unq.uis.carmensandiego;
 
+import android.support.annotation.ArrayRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,19 +15,30 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.layout_arrest_order);
         setTitle("Estas en: Egipto!");
 
-        pupulateSpinner();
+        populateVillainsSpinner();
     }
 
-    private void pupulateSpinner() {
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerVillains);
+    private void populateConnectionsSpinner() {
+        populateSpinner(R.id.spinnerConnections, R.array.connections_array);
+    }
+
+    private void populateVillainsSpinner() {
+        populateSpinner(R.id.spinnerVillains, R.array.villains_array);
+    }
+
+    private void populateSpinner(int spinnerId, @ArrayRes int arrayRes) {
+        Spinner spinner = (Spinner) findViewById(spinnerId);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.villains_array, android.R.layout.simple_spinner_item);
+                arrayRes, android.R.layout.simple_spinner_item);
+
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
 
     public void buttonViajarOnClick(View view) {
         setContentView(R.layout.layout_travel);
+        populateConnectionsSpinner();
     }
 
     public void buttonPedirPistaOnLick(View view) {
@@ -35,6 +47,6 @@ public class GameActivity extends AppCompatActivity {
 
     public void buttonEmitirOrdenOnClick(View view) {
         setContentView(R.layout.layout_arrest_order);
-        pupulateSpinner();
+        populateVillainsSpinner();
     }
 }
