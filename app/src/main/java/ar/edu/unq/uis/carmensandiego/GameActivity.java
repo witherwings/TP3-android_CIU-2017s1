ae29734e263c82e026b51816533642eb8731aae0
Lucas <lucas.wfh@gmail.com>
2017-06-21 20:47:42 -0300

    get villanos

--- app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
+++ app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
@@ -3,10 +3,22 @@ package ar.edu.unq.uis.carmensandiego;
 import android.support.annotation.ArrayRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unq.uis.carmensandiego.httpService.CarmenService;
+import ar.edu.unq.uis.carmensandiego.httpService.ServiceConnection;
+import ar.edu.unq.uis.carmensandiego.model.Villano;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class GameActivity extends AppCompatActivity {
     @Override
@@ -19,19 +31,37 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void populateConnectionsSpinner() {
-        populateSpinner(R.id.spinnerConnections, R.array.connections_array);
+        populateSpinner(R.id.spinnerConnections, ArrayAdapter.createFromResource(this,
+                R.array.connections_array, android.R.layout.simple_spinner_item));
     }
 
     private void populateVillainsSpinner() {
-        populateSpinner(R.id.spinnerVillains, R.array.villains_array);
+        //populateSpinner(R.id.spinnerVillains, R.array.villains_array);
+
+        CarmenService service = new ServiceConnection().createService();
+
+        final GameActivity that = this;
+
+        service.getVillanos(new Callback<List<Villano>>() {
+            @Override
+            public void success(List<Villano> villanos, Response response) {
+                // TODO: refactorizar
+                List<String> villanosNombre = new ArrayList<String>();
+                for (Villano v : villanos) {
+                    villanosNombre.add(v.getName());
+                }
+                populateSpinner(R.id.spinnerVillains, new ArrayAdapter(that, android.R.layout.simple_spinner_item, villanosNombre));
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                // TODO: mostrar algo lindo
+            }
+        });
     }
 
-    private void populateSpinner(int spinnerId, @ArrayRes int arrayRes) {
+    private void populateSpinner(int spinnerId, ArrayAdapter adapter) {
         Spinner spinner = (Spinner) findViewById(spinnerId);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                arrayRes, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
