71a2422a7ac8454001744de17be11667c75562f4
Lucas <lucas.wfh@gmail.com>
2017-06-22 19:04:12 -0300

    llenado solo con conexiones y comentarios

--- app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
+++ app/src/main/java/ar/edu/unq/uis/carmensandiego/GameActivity.java
@@ -38,6 +38,9 @@ public class GameActivity extends AppCompatActivity {
         startGame();
     }
 
+    /**
+     * pide un caso al servidor y se lo asigna a this.game
+     */
     private void startGame() {
         final GameActivity that = this;
 
@@ -55,6 +58,9 @@ public class GameActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * llena el spinnerConnections con los paises conectados a this.game.getPais
+     */
     private void populateConnectionsSpinner() {
         final GameActivity that = this;
 
@@ -63,8 +69,9 @@ public class GameActivity extends AppCompatActivity {
             public void onResponse(Call<List<Pais>> call, Response<List<Pais>> response) {
                 List<String> paisesNombre = new ArrayList<String>();
                 for (Pais p : response.body()) {
-                    // TODO: agregar un if para solo meter las conexiones con el pais actual
-                    paisesNombre.add(p.getName());
+                    if (isCountryConnected(p)) {
+                        paisesNombre.add(p.getName());
+                    }
                 }
                 populateSpinner(R.id.spinnerConnections, new ArrayAdapter(that, android.R.layout.simple_spinner_item, paisesNombre));
             }
@@ -76,6 +83,17 @@ public class GameActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * @return dice si p esta conectado a game.getPais()
+     */
+    private boolean isCountryConnected(Pais p) {
+        return game.getPais().getConnectedCountries().contains(p);
+    }
+
+    /**
+     * llena spinnerVillains con todos los villanos disponibles en el
+     * servidor
+     */
     private void populateVillainsSpinner() {
         final GameActivity that = this;
 
@@ -96,21 +114,36 @@ public class GameActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * llena un spinner
+     *
+     * @param spinnerId el id del spinner a llenar
+     * @param adapter los datos a cargar en el spinner
+     */
     private void populateSpinner(int spinnerId, ArrayAdapter adapter) {
         Spinner spinner = (Spinner) findViewById(spinnerId);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
     }
 
+    /**
+     * va a la layout para viajar
+     */
     public void buttonViajarOnClick(View view) {
         setContentView(R.layout.layout_travel);
         populateConnectionsSpinner();
     }
 
+    /**
+     * va a la layout para pedir pista
+     */
     public void buttonPedirPistaOnLick(View view) {
         setContentView(R.layout.layout_clue);
     }
 
+    /**
+     * va a la layout para emitir orden de arresto
+     */
     public void buttonEmitirOrdenOnClick(View view) {
         setContentView(R.layout.layout_arrest_order);
         populateVillainsSpinner();
